3a1d6c4c2af96b75f27636cdb20ffa63aa9026e7
Marko Radulovic <upss070288@gmail.com>
2020-08-05 15:28:24 +0200

    Structure created

---
+++ shortcode-form.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              www.samplelink.com
+ * @since             1.0.0
+ * @package           shortcode-form
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Shortcode Form
+ * Plugin URI:        www.samplelink.com
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Marko <mradulovic988@gmail.com>
+ * Author URI:        www.samplelink.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       shortcode-form
+ * Domain Path:       /languages
+ */
